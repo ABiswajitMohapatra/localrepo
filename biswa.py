2e8249c10401baf09965dc0836b67778bc1b7f81
Biswajit <biswajitmohapatrachem170@gmail.com>
2026-07-16 13:00:57 +0530

    New comment added

--- biswa.py
+++ biswa.py
@@ -1,1 +1,18 @@
-print("hello wordl")
+# Check prime number
+
+num = 17
+
+is_prime = True
+
+if num <= 1:
+    is_prime = False
+else:
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+if is_prime:
+    print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
